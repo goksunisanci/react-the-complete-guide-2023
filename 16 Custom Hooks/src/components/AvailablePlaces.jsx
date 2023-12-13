57f3492f1338a12eda3761348266f9529887c50f
Göksu Nişancı <nisanci.goksu@gmail.com>
2023-12-13 17:30:55 +0300

    16 Custom Hooks lesson added.

---
+++ 16 Custom Hooks/src/components/AvailablePlaces.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
+import useFetch from '../hooks/useFetch.js';
+
+const fetchedSortedPlaces = async () => {
+  const places = await fetchAvailablePlaces()
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      )
+      resolve(sortedPlaces)
+    })
+  })
+}
+
+
+export default function AvailablePlaces({ onSelectPlace }) {
+
+  const {
+    isFetching,
+    fetchedData: availablePlaces,
+    setFetchedData: setAvailablePlaces,
+    error
+  } = useFetch(fetchedSortedPlaces, [])
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
